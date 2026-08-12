09f215d0f470be1acf830221cc7e79f255c30142
Sontara444 <sontararajput@gmail.com>
2026-08-12 23:18:21 +0530

    style: modernize footer layout

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,43 +2,46 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <>
-      <footer className="footer">
-        <div className="footer-container container">
-          <div className="footer-content">
-            <div className="footer-left-content">
-              <h2 className="footer-head">SONTARA RAJPUT</h2>
-              <p className="footer-sub-head">
-                {" "}
-                A Frontend focused Web Developer building the Frontend of
-                Websites and Web Applications that leads to the success of the
-                overall product
-              </p>
-            </div>
+  const currentYear = new Date().getFullYear();
 
-            <div className="footer-right-content">
-              <h2 className="footer-head-2">SOCIAL</h2>
-              <div className="footer-icons">
-                <a target="_blank" href="/" className="footer-icon">
-                  <i className="fa-brands fa-linkedin fa-3x"></i>
-                </a>
-                <a target="_blank" href="/" className="footer-icon">
-                  <i className="fa-brands fa-instagram fa-3x"></i>
-                </a>
-                <a target="_blank" href="/" className="footer-icon">
-                  <i className="fa-brands fa-twitter fa-3x"></i>
-                </a>
-                <a target="_blank" href="/" className="footer-icon">
-                  <i className="fa-brands fa-github fa-3x"></i>
-                </a>
-              </div>
+  return (
+    <footer className="footer">
+      <div className="footer-container container">
+        <div className="footer-content">
+          <div className="footer-left-content">
+            <h2 className="footer-head">SONTARA RAJPUT</h2>
+            <p className="footer-sub-head">
+              A Frontend focused Web Developer building the Frontend of
+              Websites and Web Applications that leads to the success of the
+              overall product.
+            </p>
+          </div>
+
+          <div className="footer-right-content">
+            <h2 className="footer-head-2">SOCIAL</h2>
+            <div className="footer-icons">
+              <a target="_blank" href="https://linkedin.com" className="footer-icon" rel="noopener noreferrer">
+                <i className="fa-brands fa-linkedin fa-2x"></i>
+              </a>
+              <a target="_blank" href="https://instagram.com" className="footer-icon" rel="noopener noreferrer">
+                <i className="fa-brands fa-instagram fa-2x"></i>
+              </a>
+              <a target="_blank" href="https://twitter.com" className="footer-icon" rel="noopener noreferrer">
+                <i className="fa-brands fa-twitter fa-2x"></i>
+              </a>
+              <a target="_blank" href="https://github.com" className="footer-icon" rel="noopener noreferrer">
+                <i className="fa-brands fa-github fa-2x"></i>
+              </a>
             </div>
           </div>
         </div>
-        <p className="footer-para">Copyright &copy; 2023, All Rights Reserved</p>
-      </footer>
-    </>
+      </div>
+      <div className="footer-bottom">
+        <p className="footer-para">
+          &copy; {currentYear} Sontara Rajput | Made with <span style={{ color: "#e25555" }}>&hearts;</span>
+        </p>
+      </div>
+    </footer>
   );
 };
 
