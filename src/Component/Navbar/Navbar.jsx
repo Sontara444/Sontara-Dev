51dd49f633fb5b78eab720a1ce31d5c319d4b420
Sontara444 <sontararajput@gmail.com>
2026-08-16 23:25:05 +0530

    refactor: replace manual Navbar state with router location

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("home");
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -22,58 +23,38 @@ const Navbar = () => {
           <div className={isOpen ? "line line3 open" : "line line3"}></div>
         </div>
         <ul className={isOpen ? "nav-items open" : "nav-items"}>
-          <li
-            onClick={() => {
-              setMenu("home");
-              setIsOpen(false);
-            }}
-          >
+          <li onClick={() => setIsOpen(false)}>
             <Link style={{ textDecoration: "none", color: "black" }} to="/">
               Home
             </Link>
-            {menu === "home" ? <hr /> : <></>}
+            {currentPath === "/" ? <hr /> : <></>}
           </li>
-          <li
-            onClick={() => {
-              setMenu("about");
-              setIsOpen(false);
-            }}
-          >
+          <li onClick={() => setIsOpen(false)}>
             <Link
               style={{ textDecoration: "none", color: "black" }}
               to="/about"
             >
               About
             </Link>
-            {menu === "about" ? <hr /> : <></>}
+            {currentPath === "/about" ? <hr /> : <></>}
           </li>
-          <li
-            onClick={() => {
-              setMenu("projects");
-              setIsOpen(false);
-            }}
-          >
+          <li onClick={() => setIsOpen(false)}>
             <Link
               style={{ textDecoration: "none", color: "black" }}
               to="/projects"
             >
               Projects
             </Link>
-            {menu === "projects" ? <hr /> : <></>}
+            {currentPath === "/projects" ? <hr /> : <></>}
           </li>
-          <li
-            onClick={() => {
-              setMenu("contact");
-              setIsOpen(false);
-            }}
-          >
+          <li onClick={() => setIsOpen(false)}>
             <Link
               style={{ textDecoration: "none", color: "black" }}
               to="/contact"
             >
               Contact
             </Link>
-            {menu === "contact" ? <hr /> : <></>}
+            {currentPath === "/contact" ? <hr /> : <></>}
           </li>
         </ul>
       </div>
